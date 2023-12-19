dcb6db6f34219b9faf7c3de5706e471e5e6e8600
Nicholas Murray <nmurr13@protonmail.com>
2023-12-18 21:45:24 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require("express")
 const app = express()
 
 
+
+
 // Greetings
 app.get('/greeting/:name', (req, res) => {
 
 	res.send("What's up! " + req.params.name);
 
 });
+
+//Tip Calculator
+// When hitting the route, the page should display how much your tip will be based 
+// on the total amount of the bill and the tip percentage. 
+// (ex. hitting '/tip/100/20' should display 20 on the page).
+
+
+
+app.get('/calculator/:total/:tipPercentage', (req, res) => {
+
+	const calc= parseInt(req.params.total) / parseInt(req.params.tipPercentage);
+    res.send("$"+calc);
+
+});
+
 
 
 app.listen(3000, () => {console.log("server is listening on port 3000")})
